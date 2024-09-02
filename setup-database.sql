cfd9adccb2eec91c332be943a4c31cc0ac32664c
bekrol <rolre205@student.otago.ac.nz>
2024-09-02 15:00:42 +1200

    Added websites to db sql

--- setup-database.sql
+++ setup-database.sql
@@ -1,9 +1,10 @@
-CREATE TABLE papers (
-  code varchar(7),
-  name varchar(50) NOT NULL,
-  PRIMARY KEY (code)
+CREATE TABLE websites (
+  webId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  siteName varchar(20) NOT NULL,
+  sitePage varchar(50),
+  url varchar(50) NOT NULL,
 );
 
-INSERT INTO papers VALUES ('COSC326','Effective Programming');
-INSERT INTO papers VALUES ('COSC349','Cloud Computing Architecture');
+INSERT INTO papers VALUES ('COSC349 Cloud computing', 'Paper info', 'https://cosc349.cspages.otago.ac.nz/');
+
 
