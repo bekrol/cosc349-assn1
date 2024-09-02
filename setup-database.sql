fba148e8c6367762ab7ddabf4c74ed98494d59b6
bekrol <rolre205@student.otago.ac.nz>
2024-09-02 13:37:52 +1200

    Functioning single VM example of PHP connecting to a MySQL database.

---
+++ setup-database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE papers (
+  code varchar(7),
+  name varchar(50) NOT NULL,
+  PRIMARY KEY (code)
+);
+
+INSERT INTO papers VALUES ('COSC326','Effective Programming');
+INSERT INTO papers VALUES ('COSC349','Cloud Computing Architecture');
+
