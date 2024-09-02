165459a71bc24687713a20fbe9e695c50d398560
bekrol <rolre205@student.otago.ac.nz>
2024-09-02 18:02:11 +1200

    Test index page for second webser vm

---
+++ reports/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head><title>Book keeper</title>
+</head>
+
+<body>
+  <center>
+  <h1>THE BOOK KEEPER</h1>
+  <p>Test page for second webserver</p>
+
+</body>
+</html>
+
