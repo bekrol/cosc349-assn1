05e35c1fc7c8606b66ea8ade2ad6548f9b97e43a
bekrol <rolre205@student.otago.ac.nz>
2024-09-02 16:42:41 +1200

    Removed insert sql

--- www/database.php
+++ www/database.php
@@ -40,13 +40,6 @@ while($row = $q->fetch()){
   echo "<tr><td>".$row["siteName"]."</td><td>".$row["sitePage"]."</td><td>".$row["siteUrl"]."</td><td>".$row["tags"]."</td></tr>\n";
 }
 
-$siteName = $_REQUEST['siteName'];
-$sitePage = $_REQUEST['sitePage'];
-$siteUrl = $_REQUEST['siteUrl'];
-$tags = $_REQUEST['tags'];
-
-$sql = "INSERT INTO websites (siteName, sitePage, siteUrl, tags) VALUES (:siteName, :sitePage, :siteUrl :tags)";
-
 ?>
 </table>
 </body>
