08e2c57f8a333716a173d8643cc8ffae9fbee904
bekrol <rolre205@student.otago.ac.nz>
2024-09-02 17:50:00 +1200

    Add link back to index from db page

--- www/database.php
+++ www/database.php
@@ -21,27 +21,29 @@ th, td {
 <p>Showing all of the books you have read:</p>
 
 <table border="1">
-<tr><th>Book title</th><th>Author</th><th>Genre</th><th>Date finished</th><th>Rating</th><th>Review</th></tr>
+  <tr><th>Book title</th><th>Author</th><th>Genre</th><th>Date finished</th><th>Rating</th><th>Review</th></tr>
 
-<?php
- 
-$db_host   = '192.168.56.12';
-$db_name   = 'fvision';
-$db_user   = 'webuser';
-$db_passwd = 'insecure_db_pw';
+  <?php
+  
+    $db_host   = '192.168.56.12';
+    $db_name   = 'fvision';
+    $db_user   = 'webuser';
+    $db_passwd = 'insecure_db_pw';
 
-$pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
+    $pdo_dsn = "mysql:host=$db_host;dbname=$db_name";
 
-$pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
+    $pdo = new PDO($pdo_dsn, $db_user, $db_passwd);
 
-$q = $pdo->query("SELECT * FROM books");
+    $q = $pdo->query("SELECT * FROM books");
 
-while($row = $q->fetch()){
-  echo "<tr><td>".$row["bookName"]."</td><td>".$row["author"]."</td><td>".$row["genre"]."</td><td>".$row["dateFinished"]."</td><td>".$row["rating"]."</td><td>".$row["review"]."</td></tr>\n";
-}
+    while($row = $q->fetch()){
+      echo "<tr><td>".$row["bookName"]."</td><td>".$row["author"]."</td><td>".$row["genre"]."</td><td>".$row["dateFinished"]."</td><td>".$row["rating"]."</td><td>".$row["review"]."</td></tr>\n";
+    }
 
-?>
-</table>
+  ?>
+  </table>
+
+  <a href="index.php">Add more books here</a>
 </body>
 </html>
 
