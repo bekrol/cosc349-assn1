1de7bc276ff2f533e7a84b813a6aaff1ad944242
bekrol <rolre205@student.otago.ac.nz>
2024-09-02 16:09:45 +1200

    Changed sql insert to include columns to avoid values not matching due to auto increment id

--- setup-database.sql
+++ setup-database.sql
@@ -3,10 +3,12 @@ CREATE TABLE websites (
   siteName varchar(20) NOT NULL,
   sitePage varchar(20),
   siteUrl varchar(50) NOT NULL,
+  tags varchar(20)
 );
 
-INSERT INTO websites VALUES ('COSC349 Cloud computing', 'Paper info', 'https://cosc349.cspages.otago.ac.nz/');
-INSERT INTO websites VALUES ('Otago University', 'Main page', 'https://www.otago.ac.nz/');
-INSERT INTO websites VALUES ('Otago University', 'Student page', 'https://www.otago.ac.nz/students/');
+INSERT INTO websites (siteName, sitePage, siteURL, tags) VALUES ('COSC349 Cloud computing', 'Paper info', 'https://cosc349.cspages.otago.ac.nz/', 'Uni');
+INSERT INTO websites (siteName, sitePage, siteURL, tags) VALUES ('Otago University', 'Main page', 'https://www.otago.ac.nz/', 'Uni');
+INSERT INTO websites (siteName, sitePage, siteURL, tags) VALUES ('Otago University', 'Student page', 'https://www.otago.ac.nz/students/', 'Uni');
+INSERT INTO websites (siteName, siteURL, tags) VALUES ('The Useless Web', 'https://theuselessweb.com/', 'Fun')
 
 
