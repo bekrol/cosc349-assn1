cffe7ab043400fdc467b0c8110055d54b241eb42
bekrol <rolre205@student.otago.ac.nz>
2024-09-02 16:19:24 +1200

    Changed variables to not null

--- setup-database.sql
+++ setup-database.sql
@@ -1,9 +1,9 @@
 CREATE TABLE websites (
   webId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   siteName varchar(100) NOT NULL,
-  sitePage varchar(100),
+  sitePage varchar(100) NOT NULL,
   siteUrl varchar(100) NOT NULL,
-  tags varchar(50)
+  tags varchar(50) NOT NULL
 );
 
 INSERT INTO websites (siteName, sitePage, siteURL, tags) VALUES ('COSC349 Cloud computing', 'Paper info', 'https://cosc349.cspages.otago.ac.nz/', 'Uni');
