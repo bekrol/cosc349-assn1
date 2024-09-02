d2b62b6e96cae802b7cc637554684779a4517040
bekrol <rolre205@student.otago.ac.nz>
2024-09-02 14:33:02 +1200

    Fixed db link syntax errors

--- www/index.php
+++ www/index.php
@@ -21,10 +21,7 @@ th, td {
 <p>Collect and store your weblinks via the form below.</p>
 <p>Add categories, key names and description to help store and manage them</p>
 
-<p>You likely now want to <a href="test-database.php">proceed to your webserver's
-database connection testing page</a>. However, note that if there is a network problem reaching the database, the database connection testing page will spend a minute or so waiting before it produces any content.</p>
-
-<a href="database.php">
+<a href="database.php">View your collection of web links here</a>
 
 </body>
 </html>
