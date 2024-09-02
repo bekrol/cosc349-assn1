95940635e165dd22167459f16e09f2003dce90bf
bekrol <rolre205@student.otago.ac.nz>
2024-09-02 15:19:13 +1200

    Added more inserts to the initial db

--- setup-database.sql
+++ setup-database.sql
@@ -1,10 +1,12 @@
 CREATE TABLE websites (
   webId int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   siteName varchar(20) NOT NULL,
-  sitePage varchar(50),
+  sitePage varchar(20),
   siteUrl varchar(50) NOT NULL,
 );
 
-INSERT INTO papers VALUES ('COSC349 Cloud computing', 'Paper info', 'https://cosc349.cspages.otago.ac.nz/');
+INSERT INTO websites VALUES ('COSC349 Cloud computing', 'Paper info', 'https://cosc349.cspages.otago.ac.nz/');
+INSERT INTO websites VALUES ('Otago University', 'Main page', 'https://www.otago.ac.nz/');
+INSERT INTO websites VALUES ('Otago University', 'Student page', 'https://www.otago.ac.nz/students/');
 
 
