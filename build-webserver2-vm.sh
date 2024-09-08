3a0f0820ffd4de24325bb4e372c19cf67a2f5ec2
bekrol <rolre205@student.otago.ac.nz>
2024-09-09 10:08:25 +1200

    Added new test web2 file

--- build-webserver2-vm.sh
+++ build-webserver2-vm.sh
@@ -5,10 +5,10 @@ apt-get install -y apache2 php libapache2-mod-php php-mysql
             
 # Change VM's webserver's configuration to use shared folder.
 # (Look inside test-website.conf for specifics.)
-cp /vagrant/test-website.conf /etc/apache2/sites-available/
+cp /vagrant/test-website2.conf /etc/apache2/sites-available/
 
 # activate our website configuration ...
-a2ensite test-website
+a2ensite test-website2
 # ... and disable the default website provided with Apache
 a2dissite 000-default
 # Reload the webserver configuration, to pick up our changes
